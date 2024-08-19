0a11f6f85a4a20b5665accc919fff89f8099c459
Stephen-Jones82 <sjones8270ss@gmail.com>
2024-08-19 16:05:27 -0700

    Add files via upload

---
+++ gross_pay.py
@@ -0,0 +1,8 @@
+name = input("Hello, what is your name?:")
+print("Hello" , name)
+print ("Today I will help you figure out your total wages. Please enter hours worked and payrate when prompted please")
+hours = input ("Enter number of hours worked: ")
+hourlywage = input("Enter payrate please: ")
+totalpay = float(hours) * float(hourlywage)
+print("Your total wages are", totalpay)
+
